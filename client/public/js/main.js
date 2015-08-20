6df422475678c0a1fac0863c8973305c30e84655
Blackstock <charlieblackstock@gmail.com>
2015-08-19 21:41:59 -0600

    added shuffle and chunk functions

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,5 +1,28 @@
-// add scripts
+function shuffle(array) {
+  var temp = array.slice();
+  var shuffledArray = [];
+  while(temp.length > 0) {
+    var index = Math.floor(Math.random() * temp.length);
+    shuffledArray.push(temp.splice(index, 1)[0]);
+  }
+  return shuffledArray;
+}
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
+function chunk(array, chunkSize) {
+  var temp = array.slice();
+  var numberOfGroups = Math.floor(array.length / chunkSize);
+  var chunkedArray = [];
+
+  while (temp.length > 0) {
+    var chunks = [];
+    for (var i = 0; i < numberOfGroups; i++) {
+      chunks[i] = temp.pop();
+    }
+    chunkedArray.push(chunks);
+  }
+  return chunkedArray;
+}
+
+var inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var shuffledArray = shuffle(inputArray);
+var chunked = chunk(shuffledArray, 3);

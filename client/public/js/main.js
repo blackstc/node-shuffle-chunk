ee686c514ad81fc6951891b7b62e9f210da10c69
Blackstock <charlieblackstock@gmail.com>
2015-08-20 21:59:09 -0600

    fixed inifinite loop in chunk function

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -10,13 +10,18 @@ function shuffle(array) {
 
 function chunk(array, chunkSize) {
   var temp = array.slice();
-  var numberOfGroups = Math.floor(array.length / chunkSize);
+  var numberOfGroups = Math.ceil(array.length / chunkSize);
   var chunkedArray = [];
 
   while (temp.length > 0) {
     var chunks = [];
     for (var i = 0; i < numberOfGroups; i++) {
-      chunks[i] = temp.pop();
+      if (temp.length / numberOfGroups < 1) {
+        chunks = temp.splice(0, temp.length);
+        break;
+      } else {
+        chunks[i] = temp.pop();
+      }
     }
     chunkedArray.push(chunks);
   }
@@ -25,4 +30,4 @@ function chunk(array, chunkSize) {
 
 var inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 var shuffledArray = shuffle(inputArray);
-var chunked = chunk(shuffledArray, 3);
+var chunked = chunk(shuffledArray, 4);
